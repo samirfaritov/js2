c037e074c611e02316eb3496050a1ba7f46fef97
Samir4ik <145694562+samirfaritov@users.noreply.github.com>
2023-09-23 20:22:40 +0500

    js2

---
+++ main.js
@@ -0,0 +1,27 @@
+let a = "admin = name"
+let b = "Джон"
+
+
+console.log(
+    a = b
+);
+
+
+let balance = 25000
+
+// harajatlar
+let car = 4480
+let food = 890
+let qoldi = 19630
+
+// foyda
+let invoice = 5500
+let stock = 4200
+
+// Qoldik 
+let totalBalance = 29330
+
+console.log(
+   // balance - car - food,
+    qoldi + invoice + stock 
+);
